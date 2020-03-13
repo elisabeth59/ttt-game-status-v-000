fef66ae266cfc4c1056848930a7c27aef2d45c7e
Elisabeth Narine <elisabethnarine@gmail.com>
2020-03-13 22:12:57 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,3 +26,13 @@ end
 def full?(board)
   board.all?{|combo| combo == "X" || combo == "O"}  
 end
+
+def draw?(board)
+  board.all? {|combo| combo == "X" || combo == "O"}
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+ end
+end 
+  
